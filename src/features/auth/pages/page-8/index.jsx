85142edf755b6f375154515f756a3c933f6e7b84
Aryan Thakur <aryan197297@gmail.com>
2023-11-13 23:23:17 +0530

    feat: add the reset password form and confirmation page

---
+++ src/features/auth/pages/page-8/index.jsx
@@ -0,0 +1,72 @@
+import {
+  ChevronLeft,
+  ChevronRight,
+  Facebook,
+  Instagram,
+  Star,
+  YouTube,
+} from "@mui/icons-material";
+import React from "react";
+import { assets } from "../../../../assets";
+
+const LoginPage8 = () => {
+  const resendEmailHandler = () => {};
+  return (
+    <main className="bg-background min-h-screen grid md:grid-cols-2 grid-cols-1 text-white">
+      <section className="flex flex-col gap-10 pb-10 min-h-screen">
+        <div className="flex justify-between items-center my-3 mx-auto md:w-[70%] w-[90%]">
+          <p className="text-xl font-bold">AMP Spot</p>
+        </div>
+        <div className="flex flex-col gap-3 md:w-[70%] w-[90%] justify-center m-auto p-7 2xl:text-[24px] text-black1">
+          <img src={assets.auth.Mail} alt="Mail" />
+          <p className="text-center text-white font-bold">
+            A reset link has been sent to john***@gmail.com.
+          </p>
+          <p className="text-center my-6">
+            Didn't receive an email?{" "}
+            <button
+              type="button"
+              className="text-blue1"
+              onClick={resendEmailHandler}
+            >
+              Resend link
+            </button>
+          </p>
+        </div>
+      </section>
+      <section
+        className="hidden md:flex min-h-full overflow-hidden"
+        style={{
+          background: `url(${assets.auth.backgrounds.bg_7})no-repeat center center/cover`,
+        }}
+      >
+        <div className="w-[79%] self-end mx-auto my-8 rounded p-[7%] backdrop-blur-xl flex flex-col gap-10 2xl:gap-14">
+          <p className="text-[20px] 2xl:text-[30px]">
+            "Music has been used for therapeutic purposes in a variety of
+            settings"
+          </p>
+          <div className="flex justify-between items-center">
+            <div className="2xl:gap-3 flex flex-col">
+              <p className="font-bold text-sm 2xl:text-[24px]">Adrin Laue</p>
+              <p className="text-sm mt-1 2xl:text-[20px]">Founder Member</p>
+            </div>
+            <div>
+              <div className="relative top-[-3px]">
+                <Star sx={{ fontSize: "1.4rem" }} />
+                <Star sx={{ fontSize: "1.4rem" }} />
+                <Star sx={{ fontSize: "1.4rem" }} />
+                <Star sx={{ fontSize: "1.4rem" }} />
+              </div>
+              <div className="flex justify-between items-center my-1">
+                <ChevronLeft className="border rounded-full scale-75 2xl:scale-100" />
+                <ChevronRight className="border rounded-full scale-75 2xl:scale-100" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default LoginPage8;
---
+++ src/features/auth/pages/page-9/index.jsx
@@ -0,0 +1,122 @@
+import { ChevronLeft, ChevronRight, Star } from "@mui/icons-material";
+import { Field, Form, Formik } from "formik";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { assets } from "../../../../assets";
+import * as yup from "yup";
+import { toast } from "react-toastify";
+import useApiMutation from "../../../../hooks/useApiMutation";
+import agent from "../../../../services/agent";
+import InputField from "../../../../components/forms/InputField";
+
+const LoginPage9 = () => {
+  const { mutate, isLoading, isSuccess, isError, error, data } = useApiMutation(
+    agent.Auth.login,
+  );
+  const navigate = useNavigate();
+
+  const initialValues = {
+    password: "",
+    confirmPassword: "",
+  };
+  const validationSchema = yup.object({
+    password: yup
+      .string()
+      .min(6, "Should Have Atleast 6 Characters")
+      .required("Enter New Password"),
+    confirmPassword: yup
+      .string()
+      .min(6, "Should Have Atelast 6 Characters")
+      .required("Confirm the new password"),
+  });
+  const onSubmitHandler = (values) => {
+    values = { ...values };
+    mutate(values);
+  };
+
+  useEffect(() => {
+    if (isSuccess && data) {
+      // toast.success(data.message);
+      navigate("/dashboard");
+    } else if (isError) {
+      const errorMessage = error?.response?.data?.message || error.message;
+      toast.error(errorMessage);
+    }
+  }, [isSuccess, isError, data, error]);
+
+  return (
+    <main className="bg-background min-h-screen grid md:grid-cols-2 grid-cols-1 text-white">
+      <section className="flex flex-col pb-10 min-h-screen">
+        <div className="w-[90%] md:w-[70%] mx-auto flex flex-col">
+          <div className="flex justify-between items-center my-3">
+            <p className="text-xl font-bold">AMP Spot</p>
+            <select id="lang" name="lang" className="bg-background text-xs">
+              <option value="English (UK)">English (UK)</option>
+              <option value="English (US)">English (US)</option>
+            </select>
+          </div>
+        </div>
+        <div className="h-full flex items-center">
+          <div className="flex flex-col gap-3 justify-center w-[70%] mx-auto">
+            <h1 className="font-bold text-xl 2xl:text-[30px]">
+              Reset Password
+            </h1>
+            <Formik
+              initialValues={initialValues}
+              validationSchema={validationSchema}
+              onSubmit={onSubmitHandler}
+            >
+              {(props) => {
+                return (
+                  <Form className="flex flex-col md:w-[80%]">
+                    <InputField
+                      label="New Password"
+                      type="password"
+                      uni="password"
+                      placeholder="New Password"
+                      required={true}
+                      className={"placeholder:text-[#B9B9B9]"}
+                      {...props}
+                      lengthWarning
+                    />
+                    <InputField
+                      label="Re enter new password"
+                      type="password"
+                      uni="confirmPassword"
+                      placeholder="Type a strong password"
+                      required={true}
+                      className={"placeholder:text-[#B9B9B9]"}
+                      {...props}
+                    />
+                    <button
+                      className="text-center p-2 rounded bg-blue1 my-3 mt-5 2xl:h-[50px]"
+                      type="submit"
+                    >
+                      Reset Password
+                    </button>
+                  </Form>
+                );
+              }}
+            </Formik>
+          </div>
+        </div>
+      </section>
+      <section
+        className="hidden md:flex min-h-full overflow-hidden items-center"
+        style={{
+          background: `url(${assets.auth.backgrounds.bg_5})no-repeat center center/cover`,
+        }}
+      >
+        <div className="w-[55%] mx-auto my-8 rounded p-[6%] backdrop-blur-xl flex flex-col gap-3 items-center">
+          <img src={assets.auth.Mail} alt="Mail" className="w-[70%] rounded" />
+          <p className="text-[24px] font-bold">Log In with QR code</p>
+          <p className="text-[20px]">
+            Scan with your mobile to log in immediately
+          </p>
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default LoginPage9;
